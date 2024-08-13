60a9a63d19f3daba5e7ad921ad3dac675e824d32
Pavel Mitrafanau <pavel0837mi@gmail.com>
2024-08-13 14:06:26 +0300

    Add: UI test-cases 16-20

--- UI/Tests/conftest.py
+++ UI/Tests/conftest.py
@@ -2,6 +2,7 @@
 import pytest
 from selenium import webdriver
 from UI.Test_data import test_data
+from UI.Pages.login_page import LoginPage
 
 
 @pytest.fixture
@@ -12,3 +13,12 @@ def driver():
     chrome.get(test_data.url)
     yield chrome
     chrome.quit()
+
+
+@pytest.fixture
+def login(driver):
+    """Fixture for initializing and quitting a Chrome WebDriver instance"""
+    lp = LoginPage(driver)
+    lp.complete_login(test_data.eml, test_data.psw)
+    lp.wait_url(driver, test_data.url_contain1)
+    assert test_data.url1 in driver.current_url
--- UI/Tests/test_conatcts.py
+++ UI/Tests/test_conatcts.py
@@ -1,8 +1,6 @@
 """Tests"""
 import time
 import pytest
-
-from UI.Pages.login_page import LoginPage
 from UI.Pages.contact_list_page import ContactListPage
 from UI.Pages.add_contact_page import AddContactPage
 from UI.Pages.contact_details_page import ContactDetailsPage
@@ -11,12 +9,8 @@ from UI.Test_data import test_data
 
 
 @pytest.mark.add_contact
-def test_add_contact(driver):
+def test_add_contact(driver, login):
     """Test add contact successfully"""
-    lp = LoginPage(driver)
-    lp.complete_login(test_data.eml, test_data.psw)
-    lp.wait_url(driver, test_data.url_contain1)
-    assert test_data.url1 in driver.current_url
 
     clp = ContactListPage(driver)
     clp.click_add_button()
@@ -41,13 +35,22 @@ def test_add_contact(driver):
     assert clp.find_row() is False
 
 
-@pytest.mark.edit_contact
-def test_edit_contact(driver):
-    """Test edit contact successfully"""
-    lp = LoginPage(driver)
-    lp.complete_login(test_data.eml, test_data.psw)
-    lp.wait_url(driver, test_data.url_contain1)
+def test_cancel_add_contact(driver, login):
+    """Test cancel add contact successfully"""
+
+    clp = ContactListPage(driver)
+    clp.click_add_button()
+    clp.wait_url(driver, test_data.url_contain2)
+    assert test_data.url2 in driver.current_url
+
+    acp = AddContactPage(driver)
+    acp.click_cancel_button()
     assert test_data.url1 in driver.current_url
+
+
+@pytest.mark.edit_contact
+def test_edit_contact(driver, login):
+    """Test edit contact successfully"""
 
     clp = ContactListPage(driver)
     clp.click_add_button()
@@ -67,6 +70,7 @@ def test_edit_contact(driver):
     cdp = ContactDetailsPage(driver)
     cdp.click_edit_button()
     cdp.wait_url(driver, test_data.url_contain4)
+
     ecp = EditContactPage(driver)
     time.sleep(1)
     ecp.edit_contact(test_data.fn_1, test_data.ln_1, test_data.bd_1,
@@ -85,13 +89,45 @@ def test_edit_contact(driver):
     assert clp.find_row() is False
 
 
+def test_cancel_edit_contact(driver, login):
+    """Test edit contact successfully"""
+
+    clp = ContactListPage(driver)
+    clp.click_add_button()
+    clp.wait_url(driver, test_data.url_contain2)
+    assert test_data.url2 in driver.current_url
+
+    acp = AddContactPage(driver)
+    acp.add_contact(test_data.fn, test_data.ln, test_data.bd,
+                    test_data.eml1, test_data.pn, test_data.str1,
+                    test_data.str2, test_data.ct, test_data.stpr,
+                    test_data.pc, test_data.cntr)
+    acp.wait_url(driver, test_data.url_contain1)
+    clp.click_first_row()
+    clp.wait_url(driver, test_data.url_contain3)
+    assert test_data.url3 in driver.current_url
+
+    cdp = ContactDetailsPage(driver)
+    cdp.click_edit_button()
+    cdp.wait_url(driver, test_data.url_contain4)
+
+    ecp = EditContactPage(driver)
+    time.sleep(1)
+    ecp.click_cancel_button()
+    ecp.wait_url(driver, test_data.url_contain3)
+    assert test_data.url3 in driver.current_url
+
+    cdp.click_delete_button()
+    alert = driver.switch_to.alert
+    alert.accept()
+    cdp.wait_url(driver, test_data.url_contain1)
+    clp = ContactListPage(driver)
+    assert clp.find_row() is False
+
+
 @pytest.mark.delete_contact
-def test_delete_contact(driver):
+def test_delete_contact(driver, login):
     """Test delete contact successfully"""
-    lp = LoginPage(driver)
-    lp.complete_login(test_data.eml, test_data.psw)
-    lp.wait_url(driver, test_data.url_contain1)
-    assert test_data.url1 in driver.current_url
 
     clp = ContactListPage(driver)
     clp.click_add_button()
