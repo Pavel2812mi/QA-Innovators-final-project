5589c2c79888c62832e2d294598dce4cbf4cd7e5
Anastasiya Solodukho <anastasiya.solodukho@gmail.com>
2024-08-23 09:13:38 +0200

    Changed dockerfile

--- UI/Tests/conftest.py
+++ UI/Tests/conftest.py
@@ -18,6 +18,8 @@ def driver():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--remote-debugging-port=9222')
     chrome = webdriver.Chrome(options=chrome_options)
     chrome.implicitly_wait(5)
     chrome.get(test_data.url)
